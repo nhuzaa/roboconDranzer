10a08fd7fb5b17b2e2bd62b932a2ea2f7a1e921a
Rohit Sthapit <nhuzaa@gmail.com>
2016-06-23 23:24:22 +0545

    3 bit state

--- lineFollower/takeGPIO.py
+++ lineFollower/takeGPIO.py
@@ -3,11 +3,13 @@ from time import sleep
 
 
 GPIO.setmode(GPIO.BCM)
-bit0Pin = 20
-bit1Pin = 21
+bit0Pin = 19
+bit1Pin = 20
+bit2Pin = 21
 # Set up the GPIO channels
 GPIO.setup(bit0Pin, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # This could be used to detect a switch
 GPIO.setup(bit1Pin, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # This is going to supply the on/off signal
+GPIO.setup(bit2Pin, GPIO.IN,pull_up_down=GPIO.PUD_DOWN) # This could be used to detect a switch
 
 # Output to pin 12. Toggles it on/off every three seconds
 while True:
@@ -16,13 +18,17 @@ while True:
 # Input from pin 11
     bit0 = GPIO.input(bit0Pin)
     bit1 = GPIO.input(bit1Pin)
+    bit2 = GPIO.input(bit2Pin)
 
-    if bit0 == 0 and bit1 == 0:
+    state = bytes(bit0) + bytes(bit1) + bytes(bit2)
+    print(state)
+    if bit2 == 0 and bit1 == 0 and bit2 == 0 :
         print(" manaul")
-    elif bit0 == 0 and bit1 == 1:
+    elif bit2 == 0 and bit1 == 0 and bit2 == 1 :
         print(" Automatic")
-    elif bit0 == 1 and bit1 == 0:
+    elif bit2 == 0 and bit1 == 1 and bit2 == 0 :
         print("Tuin Point")
-    elif bit0 == 1 and bit1 == 1:
+    elif bit2 == 0 and bit1 == 1 and bit2 == 1 :
         print(" Salt Point")
 
+    sleep(2)
